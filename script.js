b161c56ce1f5654eda1277144cde0cedd88eb4bf
3C Projetos <00001245454833sp@al.educacao.sp.gov.br>
2025-08-29 21:12:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-document.getElementById("feedbackForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    document.getElementById("msgSucesso").style.display = "block";
-    this.reset();
-  });
-  
+// Alternar menu acessibilidade
+document.getElementById("botao-acessibilidade").addEventListener("click", () => {
+    document.getElementById("opcoes-acessibilidade").classList.toggle("mostrar");
+});
+
+// Aumentar fonte
+document.getElementById("aumentar-fonte").addEventListener("click", () => {
+    document.body.style.fontSize = "larger";
+});
+
+// Diminuir fonte
+document.getElementById("diminuir-fonte").addEventListener("click", () => {
+    document.body.style.fontSize = "smaller";
+});
